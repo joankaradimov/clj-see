bb0fd6ff865c2a42891e6f48efdc2ec12b343fb4
Joan Karadimov <joan.karadimov@gmail.com>
2013-05-22 22:27:24 +0300

    Implement utility functions for genetic operations

--- src/clj_see/core.clj
+++ src/clj_see/core.clj
@@ -1,1 +1,17 @@
 (ns clj-see.core)
+
+(defn prepend-paths [index paths]
+  (if (zero? index)
+    [()]
+    (for [path paths] (cons index path))))
+
+(defn flatten-1 [list]
+  (apply concat list))
+
+(defn all-paths [expression]
+  (if (list? expression)
+    (->> expression
+         (map all-paths)
+         (map-indexed prepend-paths)
+         flatten-1)
+    [()]))
--- test/clj_see/core_test.clj
+++ test/clj_see/core_test.clj
@@ -1,3 +1,19 @@
 (ns clj-see.core-test
   (:require [clojure.test :refer :all]
             [clj-see.core :refer :all]))
+
+(deftest test-prepend-paths
+  (is (= (prepend-paths 3 '[()]) '[(3)]))
+  (is (= (prepend-paths 3 '[() (1) (1 2 3)]) '[(3) (3 1) (3 1 2 3)])))
+
+(deftest test-flatten-1
+  (is (= (flatten-1 '((1) (1 2) (2))) '(1 1 2 2)))
+  (is (= (flatten-1 '((1) ((1 2)) (2))) '(1 (1 2) 2))))
+
+(deftest test-all-paths
+  (is (= (all-paths '42) '[()]))
+  (is (= (all-paths '(inc 42)) '[() (1)]))
+  (is (= (all-paths '(+ 42 35)) '[() (1) (2)]))
+  (is (= (all-paths '(+ 42 (something))) '[() (1) (2)]))
+  (is (= (all-paths '(+ 42 (something else))) '[() (1) (2) (2 1)]))
+  (is (= (all-paths '(+ 42 (* 7 8))) '[() (1) (2) (2 1) (2 2)])))
