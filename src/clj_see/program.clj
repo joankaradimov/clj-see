9ef954ccc6acc99c4959faca4c61f3c9efb07d61
Joan Karadimov <joan.karadimov@gmail.com>
2013-07-04 23:45:11 +0300

    Remove some unneeded call indirection

--- src/clj_see/program.clj
+++ src/clj_see/program.clj
@@ -4,19 +4,25 @@
 (defn create-program [expression]
   {:expression expression})
 
-(defn all-paths [program]
-  (-> program :expression clj-see.util/all-paths))
-
 (defn expression [program]
   (program :expression))
 
+(defn random-path [program]
+  (-> program expression clj-see.util/all-paths rand-nth))
+
 (defn program->fn [program]
-  (clj-see.expression/expression->fn (program :expression)))
+  (eval `(fn [~'r] ~(program :expression))))
 
 (defn crossover [program-1 program-2]
   (map create-program
        (clj-see.expression/crossover (expression program-1)
-                                     (expression program-2))))
+                                     (random-path program-1)
+                                     (expression program-2)
+                                     (random-path program-2))))
 
 (defn mutate [program mutate-fn]
-  (create-program (clj-see.expression/mutate (expression program) mutate-fn)))
+  (let [expression (expression program)
+        path (random-path program)]
+    (-> expression
+        (clj-see.expression/mutate path mutate-fn)
+        create-program)))
