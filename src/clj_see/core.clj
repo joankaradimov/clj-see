dce256f5bfdf1ba943b852ba62f39b8bfa5e2f3d
Joan Karadimov <joan.karadimov@gmail.com>
2013-07-07 09:26:37 +0300

    Implement a rudimentary population persitence

--- src/clj_see/core.clj
+++ src/clj_see/core.clj
@@ -3,13 +3,35 @@
             [clj-see.program :as program]
             [examples.circle-area]))
 
+(def population-filename "circle-area.txt")
+
+(defn load-population [filename]
+  (-> filename
+      slurp
+      population/deserialize))
+
+(defn dump-population [filename population]
+  (->> population
+       population/serialize
+       (spit filename)))
+
+(defn load-or-create-population [filename]
+  (try
+    (let [population (load-population filename)]
+      (println "Loaded population from file")
+      population)
+    (catch Exception e
+      (println "Created a new population")
+      (population/create-population 30))))
+
 (defn -main []
-  (loop [population (population/create-population 30)
+  (loop [population (load-or-create-population population-filename)
          iteration 0]
     (if (< iteration 80)
       (let [next-gen population/next-generation
             new-population (next-gen population
                                      examples.circle-area/fitness
                                      0.1)]
+        (dump-population population-filename new-population)
         (recur new-population (inc iteration)))
       (-> population population/serialize print))))
