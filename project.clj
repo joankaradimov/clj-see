17c38bfdc37ecc8638fed6ae33da1cdbc150604c
Joan Karadimov <joan.karadimov@gmail.com>
2013-07-12 21:45:03 +0300

    Remove an unused dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,5 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.combinatorics "0.0.4"]
                  [org.clojure/math.numeric-tower "0.0.2"]]
   :main clj-see.core)
