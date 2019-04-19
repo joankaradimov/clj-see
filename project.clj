485de46e36c6b083e0d58f8e554be56c7c93d4b8
Joan Karadimov <joan.karadimov@gmail.com>
2019-04-20 00:36:55 +0300

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/joankaradimov/clj-see"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/math.numeric-tower "0.0.2"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :main clj-see.core)
