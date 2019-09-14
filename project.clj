a4cb2bb5929be1e4b26ea096106cf05461e76d9c
Joan Karadimov <joan@profuzdigital.com>
2019-09-14 16:36:30 +0300

    Move all clojure code to a 'clojure' subdirectory

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [me.raynes/fs "1.4.6"]]
+  :source-paths ["src/clojure"]
   :main clj-see.core)
